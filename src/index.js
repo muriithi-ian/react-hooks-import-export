619278e8e9f447450e552076c762d2756ff1ae48
Muriithi Ian <imuriithiian@gmail.com>
2022-09-23 15:38:30 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
+import MesaVerde from "./parks/MesaVerde";
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+ReactDOM.render(
+	[<ColoradoStateParks />, <MesaVerde />],
+	document.getElementById("root")
+);
